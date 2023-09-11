5c07c1abd8a76aae4861f4cb3488a47537f18fbd
indaegu <129815040+indaegu@users.noreply.github.com>
2023-09-11 15:17:28 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 11.11 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -9,3 +9,5 @@ class Solution {
         return answer = num1>=num2 ? num1:num2;
     }
 }
+
+//회고 : 삼항연산자 대신 return Math.max(Integer.parseInt(String.valueOf(a)+String.valueOf(b)),2*a*b);를 사용하면 더 간단하게 가능했다. 해당 방법은 String.valueOf로 문자열형태로 a값을 가져온뒤 두 문자열을 합하고 parseInt로 다시 정수형으로 변환하여 최대값을 구하는 방식이다.
