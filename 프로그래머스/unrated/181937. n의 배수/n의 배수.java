2a2d7876450cd608cf4d31b7ad4d4bf5101dd173
indaegu <129815040+indaegu@users.noreply.github.com>
2023-09-14 16:52:04 +0900

    [unrated] Title: n의 배수, Time: 0.02 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -0,0 +1,7 @@
+// 전략 : 삼항연산자로 num%n의 값이 0이면 1을 아니면 0을 출력하도록한다. 
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        return answer = (num%n ==0) ? 1 :0;
+    }
+}
