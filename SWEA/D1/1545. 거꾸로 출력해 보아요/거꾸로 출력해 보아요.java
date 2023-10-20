57a922a8aa9591ea20e4dcb1ced57c9f4fdb2733
indaegu <129815040+indaegu@users.noreply.github.com>
2023-10-20 14:52:52 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 131 ms, Memory: 20,388 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = N; i >= 0; i--) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
+
+
