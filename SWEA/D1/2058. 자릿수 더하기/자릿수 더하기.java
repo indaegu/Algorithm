6dcc7a58b8ca53c3c386f42fc50559da0e875a25
indaegu <129815040+indaegu@users.noreply.github.com>
2023-10-31 15:46:22 +0900

    [D1] Title: 자릿수 더하기, Time: 143 ms, Memory: 20,732 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -1,49 +1,16 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;
-// double b = 1.0;
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
 import java.io.FileInputStream;
+import java.net.Proxy;
+import java.util.Scanner;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-public class Solution
-{
-  public static void main(String args[]) throws Exception
-  {
-      Scanner sc = new Scanner(System.in);
-      int k = sc.nextInt(); //int 형 입력 받음 ex)123
-      String num = Integer.toString(k); // 123을 String형으로 변환
-      int sum = 0; // 합을 담을 int 선언
-      for(int i =1; i<=num.length();i++){ // string의 문자열 길이만큼 반복 ex) 3번반복
-        sum += Character.getNumericValue(num.charAt(num.length()-i));
-        // String에서 맨뒤문자부터 char형으로 꺼내옴 -> 해당 문자를 숫자로 바꾸고 sum에 더함
-      }
+public class Solution {
+  public static void main(String args[]) throws Exception {
+    //System.setIn(new FileInputStream("src/input.txt"));
+    Scanner sc = new Scanner(System.in);
+    String N = sc.next();
+    int sum = 0;
+    for (int i = 0; i < N.length(); i++) {
+      sum += Integer.parseInt(String.valueOf(N.charAt(i)));
+    }
     System.out.println(sum);
   }
 }
