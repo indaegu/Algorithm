d6dfb7f95f6c9d400eccb58ac0c16ed6cf16f5c2
indaegu <129815040+indaegu@users.noreply.github.com>
2023-10-31 15:23:18 +0900

    [D1] Title: 평균값 구하기, Time: 130 ms, Memory: 20,340 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -1,26 +1,17 @@
 import java.io.FileInputStream;
-import java.util.Arrays;
 import java.util.Scanner;
 
-import static java.lang.Math.ceil;
-import static java.lang.Math.round;
-
 public class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int num = 0;
-        for (int i = 1; i <= T; i++) {
-            double sum =0;
-            System.out.printf("#%d ",i);
-            for (int j = 0; j < 10; j++) {
-                num = sc.nextInt();
-                sum += num;
-            }
-            
-            System.out.printf("%d\n",round(sum/10));
-        }
+  public static void main(String args[]) throws Exception {
+    //System.setIn(new FileInputStream("src/input.txt"));
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for (int test_case = 1; test_case <= T; test_case++) {
+      int sum= 0;
+      for (int i = 0; i < 10; i++) {
+            sum += sc.nextInt();
+      }
+      System.out.println("#" + test_case + " " + Math.round((float) sum /10));
     }
+  }
 }
-
-
