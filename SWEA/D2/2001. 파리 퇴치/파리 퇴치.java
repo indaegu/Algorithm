67150ba83379f71a572c6f4ed4be18e4841426ba
indaegu <129815040+indaegu@users.noreply.github.com>
2023-11-06 10:55:59 +0900

    [D2] Title: 파리 퇴치, Time: 162 ms, Memory: 23,552 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -2,36 +2,37 @@ import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class Solution {
-  public static void main(String args[]) throws Exception {
-    //System.setIn(new FileInputStream("src/input.txt"));
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-    for (int test_case = 1; test_case <= T; test_case++) {
-      int N = sc.nextInt();
-      int M = sc.nextInt();
-      int[][] Marr = new int[N][N];
-      for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-          Marr[i][j] = sc.nextInt();
+    public static void main(String args[]) throws Exception {
+        //System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            System.out.print("#" + test_case + " ");
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int[][] arr = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            int max = 0;
+            for (int h = 0; h < N; h++) {
+                for (int i = 0; i < N; i++) {
+                    int sum = 0;
+                    for (int j = 0; j < M; j++) {
+                        if(j+h>=N) continue;
+                        for (int k = 0; k < M; k++) {
+                            if(k+i>=N) continue;
+                            sum += arr[j+h][k+i];
+                        }
+                    }
+                    if(max <= sum){
+                        max = sum;
+                    }
+                }
+            }
+            System.out.println(max);
         }
-      }
-      System.out.println("#" + test_case + " " + getMaxFlies(Marr, N, M));
     }
-  }
-
-  public static int getMaxFlies(int[][] Marr, int N, int M) {
-    int maxFlies = 0;
-    for (int i = 0; i <= N - M; i++) { // M*M 배열의 시작점을 지정하기 위한 2중 반복분
-      for (int j = 0; j <= N - M; j++) {
-        int flies = 0;
-        for (int k = i; k < i + M; k++) { // M*M 배열의 크기 만큼 수색을 진행할 반복문
-          for (int l = j; l < j + M; l++) {
-            flies += Marr[k][l];
-          }
-        }
-        maxFlies = Math.max(maxFlies, flies);
-      }
-    }
-    return maxFlies;
-  }
 }
