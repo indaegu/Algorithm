82a7b5139d95c31039a64fc8dfc33a01cecfd461
indaegu <129815040+indaegu@users.noreply.github.com>
2023-11-09 16:30:48 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 205 ms, Memory: 36,312 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -2,45 +2,32 @@ import java.io.FileInputStream;
 import java.util.*;
 
 public class Solution {
-  public static void main(String args[]) throws Exception {
-    //System.setIn(new FileInputStream("src/input.txt"));
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-    for (int test_case = 1; test_case <= T; test_case++) {
-      int N = sc.nextInt();
-      ArrayList<Integer> arrayList = new ArrayList<>();
-      for (int i = 0; i < 1000; i++) {
-        arrayList.add(sc.nextInt());
-      }
-      System.out.println("#" + N + " " + method(arrayList));
+    public static void main(String args[]) throws Exception {
+        //System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int testCaseNum = sc.nextInt();
+            System.out.print("#" + testCaseNum + " ");
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            for (int i = 0; i < 1000; i++) {
+                arrayList.add(sc.nextInt());
+            }
+            int[] arr = new int[101];
+            for (int i = 0; i < 1000; i++) {
+                if(arrayList.contains(arrayList.get(i))){
+                    arr[arrayList.get(i)] += 1;
+                }
+            }
+            int max = 0;
+            int answer =0;
+            for (int i = 0; i < 100; i++) {
+                if(max <= arr[i]){
+                    max = arr[i];
+                    answer = i;
+                }
+            }
+            System.out.println(answer);
+        }
     }
-  }
-
-  public static int method(ArrayList<Integer> arrayList) {
-    Map<Integer,Integer> map = new HashMap<>();
-    int max = 0;
-    int maxx =0;
-    for (int i = 0; i < 1000; i++) {
-      if(map.containsKey(arrayList.get(i))){
-        map.put(arrayList.get(i), map.get(arrayList.get(i))+1);
-      }else{
-        map.put(arrayList.get(i), 1);
-      }
-    }
-    //참조 부분
-    int maxCount = 0;
-    int maxNumber = 0;
-    // entryset은 map의 모든 키-값 집합을 반환
-    // 하나씩 entry를 비교하며 반복문 수행
-    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-      if (entry.getValue() > maxCount) { // 각 entry의 value를 뽑아 내면서 가장 큰 수이면 max count와 number를 초기화
-        maxCount = entry.getValue();
-        maxNumber = entry.getKey();
-      } else if (entry.getValue() == maxCount && entry.getKey() > maxNumber) { // max count가 동일하면 key 값이 더큰 수를 출력
-        maxNumber = entry.getKey();
-      }
-    }
-
-    return maxNumber;
-  }
 }
