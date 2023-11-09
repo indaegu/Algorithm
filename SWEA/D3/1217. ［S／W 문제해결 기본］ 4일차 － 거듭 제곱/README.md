fc5cd8d3a21cd9113dc996421b691954b13fe35e
indaegu <129815040+indaegu@users.noreply.github.com>
2023-11-09 11:43:33 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 150 ms, Memory: 20,808 KB -BaekjoonHub

---
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/README.md
@@ -0,0 +1,15 @@
+# [D3] [S/W 문제해결 기본] 4일차 - 거듭 제곱 - 1217 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14dUIaAAUCFAYD) 
+
+### 성능 요약
+
+메모리: 20,808 KB, 시간: 150 ms, 코드길이: 720 Bytes
+
+### 제출 일자
+
+2023-11-09 11:43
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[]) throws Exception {
+        //System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            int testCaseNum = sc.nextInt();
+            System.out.print("#" + testCaseNum + " ");
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            System.out.println(sqrt(N,M));
+        }
+    }
+    public static int sqrt(int N, int M){
+        int answer = 1;
+        for (int i = 0; i < M; i++) {
+            answer = answer * N;
+        }
+        return answer;
+    }
+}
