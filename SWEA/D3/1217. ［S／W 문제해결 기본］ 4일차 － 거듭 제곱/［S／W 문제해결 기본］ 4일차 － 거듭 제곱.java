afbd8bc21c44103da8403f88d7b3847f8ca2f59a
indaegu <129815040+indaegu@users.noreply.github.com>
2023-11-09 11:45:57 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 128 ms, Memory: 20,360 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String args[]) throws Exception {
-        //System.setIn(new FileInputStream("src/input.txt"));
+       // System.setIn(new FileInputStream("src/input.txt"));
         Scanner sc = new Scanner(System.in);
         for (int test_case = 1; test_case <= 10; test_case++) {
             int testCaseNum = sc.nextInt();
@@ -13,11 +13,17 @@ public class Solution {
             System.out.println(sqrt(N,M));
         }
     }
+//    public static int sqrt(int N, int M){
+//        int answer = 1;
+//        for (int i = 0; i < M; i++) {
+//            answer = answer * N;
+//        }
+//        return answer;
+//    }
     public static int sqrt(int N, int M){
-        int answer = 1;
-        for (int i = 0; i < M; i++) {
-            answer = answer * N;
-        }
-        return answer;
+        // 종료 조건
+        if (M == 0) return 1;
+        // 재귀 조건
+        return N * sqrt(N, M - 1);
     }
 }
