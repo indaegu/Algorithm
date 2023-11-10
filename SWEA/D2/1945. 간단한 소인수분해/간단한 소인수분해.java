2bd62a0ccf49db5dcecf8dda6b309db4a43e72c7
indaegu <129815040+indaegu@users.noreply.github.com>
2023-11-10 15:34:26 +0900

    [D2] Title: 간단한 소인수분해, Time: 136 ms, Memory: 21,208 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -2,44 +2,37 @@ import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int num = sc.nextInt();
-            int a = 0;
-            int b = 0;
-            int c = 0;
-            int d = 0;
-            int e = 0;
-            System.out.printf("#%d ", test_case);
-
-            while (num > 0){
-                if(num % 2 ==0){
-                    num /= 2;
-                    a++;
-                }else if(num % 3 == 0){
-                    num /= 3;
-                    b++;
-                }else if(num % 5 == 0){
-                    num /= 5;
-                    c++;
-                }
-                else if(num % 7 == 0){
-                    num /= 7;
-                    d++;
-                }
-                else if(num % 11 == 0){
-                    num /= 11;
-                    e++;
-                }else{
-                    break;
-                }
-            }
-            System.out.printf("%d %d %d %d %d\n", a, b, c, d, e);
+  public static void main(String args[]) throws Exception {
+    //System.setIn(new FileInputStream("src/input.txt"));
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for (int test_case = 1; test_case <= T; test_case++) {
+      System.out.print("#" + test_case + " ");
+      int N = sc.nextInt();
+      int a = 0;
+      int b = 0;
+      int c = 0;
+      int d = 0;
+      int e = 0;
+      while(N>1){
+        if(N%2==0){
+          N/=2;
+          a++;
+        }else if(N%3==0) {
+          N/=3;
+          b++;
+        }else if(N%5==0) {
+          N/=5;
+          c++;
+        }else if(N%7==0) {
+          N/=7;
+          d++;
+        }else if(N%11==0) {
+          N/=11;
+          e++;
         }
+      }
+      System.out.printf("%d %d %d %d %d\n",a,b,c,d,e);
     }
-
+  }
 }
